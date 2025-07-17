2c40c9f4baac52bdff213d9f13ecc92cec8f7f1b
hasnain17576 <hasnainwasli17@gmail.com>
2025-07-17 17:18:40 +0500

    submit

---
+++ Javasccript code/first.js
@@ -0,0 +1,70 @@
+ 
+ 
+// //  variable declaration
+// // variable case sensitivity
+// let fullName;
+// let fullname;
+
+// console.log(fullName);
+// console.log(fullname);
+
+
+
+
+// fullName = ('hasnain wasli');
+// console.log(fullName);
+// console.log(fullName.length);
+// console.log(fullName.toUpperCase());
+// console.log(fullName.toLowerCase());
+// console.log(fullName.indexOf('wasli'));
+// console.log(fullName.lastIndexOf('wasli'));
+// console.log(fullName.slice(0, 7));
+// console.log(fullName.slice(8));
+// console.log(fullName.replace('wasli', 'wasli khan'));
+// console.log(fullName.split(' '));
+// console.log(fullName.split(' '));
+// console.log(fullName.split('a'));
+// console.log(fullName.split(''));
+// console.log(fullName.trim());
+// console.log(fullName.charAt(0));
+// console.log(fullName.charAt(1));
+// console.log(fullName.charAt(2));
+// console.log(fullName.charAt(3));
+// console.log(fullName.charAt(4));
+// console.log(fullName.charAt(5));
+// console.log(fullName.charAt(6));
+// console.log(fullName.charAt(7));
+// console.log(fullName.charAt(8));
+// console.log(fullName.charAt(9));
+// console.log(fullName.charAt(10));
+// console.log(fullName.charAt(11));
+// console.log(fullName.charAt(12));
+// console.log(fullName.charAt(13));
+// isFollow = true;
+// console.log(isFollow);
+// console.log(typeof isFollow);
+// console.log(typeof fullName);
+// console.log(typeof 5);
+// // assignment operator =
+// // let isFollow;
+// // isFollow = true;
+// // console.log(isFollow);
+
+
+
+// // variable declaration
+// let isFollow;
+// isFollow = true;
+// console.log(isFollow);
+// // let variable declaration 
+
+// // var variable declaration
+// // const variable declaration
+
+const student = {
+    fullName: 'Hasnain Wasli',
+    age: 20,
+    location: 'Pakistan',
+    skills: ['JavaScript', 'Python', 'Java'],
+
+};
