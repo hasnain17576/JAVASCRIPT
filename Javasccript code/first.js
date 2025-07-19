9b258375e0f1eb280c1267789e41a11cffb24fd0
hasnain17576 <hasnainwasli17@gmail.com>
2025-07-19 15:55:09 +0500

    submitt

--- Javasccript code/first.js
+++ Javasccript code/first.js
@@ -1,70 +1,246 @@
  
  
-// //  variable declaration
-// // variable case sensitivity
-// let fullName;
-// let fullname;
+// // // //  variable declaration
+// // // // variable case sensitivity
+// // // let fullName;
+// // // let fullname;
 
-// console.log(fullName);
-// console.log(fullname);
-
-
-
-
-// fullName = ('hasnain wasli');
-// console.log(fullName);
-// console.log(fullName.length);
-// console.log(fullName.toUpperCase());
-// console.log(fullName.toLowerCase());
-// console.log(fullName.indexOf('wasli'));
-// console.log(fullName.lastIndexOf('wasli'));
-// console.log(fullName.slice(0, 7));
-// console.log(fullName.slice(8));
-// console.log(fullName.replace('wasli', 'wasli khan'));
-// console.log(fullName.split(' '));
-// console.log(fullName.split(' '));
-// console.log(fullName.split('a'));
-// console.log(fullName.split(''));
-// console.log(fullName.trim());
-// console.log(fullName.charAt(0));
-// console.log(fullName.charAt(1));
-// console.log(fullName.charAt(2));
-// console.log(fullName.charAt(3));
-// console.log(fullName.charAt(4));
-// console.log(fullName.charAt(5));
-// console.log(fullName.charAt(6));
-// console.log(fullName.charAt(7));
-// console.log(fullName.charAt(8));
-// console.log(fullName.charAt(9));
-// console.log(fullName.charAt(10));
-// console.log(fullName.charAt(11));
-// console.log(fullName.charAt(12));
-// console.log(fullName.charAt(13));
-// isFollow = true;
-// console.log(isFollow);
-// console.log(typeof isFollow);
-// console.log(typeof fullName);
-// console.log(typeof 5);
-// // assignment operator =
-// // let isFollow;
-// // isFollow = true;
-// // console.log(isFollow);
+// // // console.log(fullName);
+// // // console.log(fullname);
 
 
 
-// // variable declaration
-// let isFollow;
-// isFollow = true;
-// console.log(isFollow);
-// // let variable declaration 
 
-// // var variable declaration
-// // const variable declaration
+// // // fullName = ('hasnain wasli');
+// // // console.log(fullName);
+// // // console.log(fullName.length);
+// // // console.log(fullName.toUpperCase());
+// // // console.log(fullName.toLowerCase());
+// // // console.log(fullName.indexOf('wasli'));
+// // // console.log(fullName.lastIndexOf('wasli'));
+// // // console.log(fullName.slice(0, 7));
+// // // console.log(fullName.slice(8));
+// // // console.log(fullName.replace('wasli', 'wasli khan'));
+// // // console.log(fullName.split(' '));
+// // // console.log(fullName.split(' '));
+// // // console.log(fullName.split('a'));
+// // // console.log(fullName.split(''));
+// // // console.log(fullName.trim());
+// // // console.log(fullName.charAt(0));
+// // // console.log(fullName.charAt(1));
+// // // console.log(fullName.charAt(2));
+// // // console.log(fullName.charAt(3));
+// // // console.log(fullName.charAt(4));
+// // // console.log(fullName.charAt(5));
+// // // console.log(fullName.charAt(6));
+// // // console.log(fullName.charAt(7));
+// // // console.log(fullName.charAt(8));
+// // // console.log(fullName.charAt(9));
+// // // console.log(fullName.charAt(10));
+// // // console.log(fullName.charAt(11));
+// // // console.log(fullName.charAt(12));
+// // // console.log(fullName.charAt(13));
+// // // isFollow = true;
+// // // console.log(isFollow);
+// // // console.log(typeof isFollow);
+// // // console.log(typeof fullName);
+// // // console.log(typeof 5);
+// // // // assignment operator =
+// // // // let isFollow;
+// // // // isFollow = true;
+// // // // console.log(isFollow);
 
-const student = {
-    fullName: 'Hasnain Wasli',
-    age: 20,
-    location: 'Pakistan',
-    skills: ['JavaScript', 'Python', 'Java'],
 
-};
+
+// // // // variable declaration
+// // // let isFollow;
+// // // isFollow = true;
+// // // console.log(isFollow);
+// // // // let variable declaration 
+
+// // // // var variable declaration
+// // // // const variable declaration
+
+// // const student = {
+// //     fullName: 'Hasnain Wasli',
+// //     age: 20,
+// //     location: 'Pakistan',
+// //     skills: ['JavaScript', 'Python', 'Java'],
+
+// // };
+
+
+
+// // this is used for single line comment
+// // console.log(student);
+
+//  /* this is used for
+//  multi line comment */
+
+
+// //  operators
+// //+
+// //-
+// //*
+// ///
+// // %
+// // console.log(5 + 5);
+// // console.log(5 - 5);
+// // console.log(5 * 5);
+// // console.log(5 / 5);
+// // console.log(5 % 5);
+// //// console.log(5 ** 2); // exponentiation operator
+// // console.log(5 ** 3); // exponentiation operator
+
+
+
+
+
+
+
+
+// //assignment operators
+// // let a = 5;
+// // let b = 10;
+// // a += 5; // a = a + 5;
+// // console.log(a); // 10
+// // b -= 5; // b = b - 5;
+// // console.log(b); // 5
+// // a *= 2; // a = a * 2;
+// // console.log(a); // 20
+// // b /= 2; // b = b / 2;
+// // console.log(b); // 2.5
+// // a %= 3; // a = a % 3;
+// // console.log(a); // 2
+// // b **= 2; // b = b ** 2;
+// // console.log(b); // 100
+// // b <<= 2; // b = b << 2;
+// // console.log(b); // 400
+// // b >>= 2; // b = b >> 2;
+// // console.log(b); // 100
+// // b &= 2; // b = b & 2;
+
+
+
+
+// //arithmetic operators
+// // let a = 5;
+// // let b = 10;
+// // console.log(a + b); // addition
+// // console.log(a - b); // subtraction
+// // console.log(a * b); // multiplication
+// // console.log(a / b); // division
+// // console.log(a % b); // modulus
+// // console.log(a ** 2); // exponentiation
+// // console.log(a++); // post increment
+// // console.log(a); // 6
+// // console.log(++a); // pre increment
+// // console.log(a--); // post decrement
+// // console.log(a); // 5
+// // console.log(--a); // pre decrement
+// // console.log(a + b); // addition
+// // console.log(a - b); // subtraction
+// // console.log(a * b); // multiplication
+// // console.log(a / b); // division
+// // console.log(a % b); // modulus
+// // console.log(a ** 2); // exponentiation
+// // console.log(a++); // post increment
+// // console.log(a); // 6
+// // console.log(++a); // pre increment
+// // console.log(a--); // post decrement
+// // console.log(a); // 5
+// // console.log(--a); // pre decrement
+
+
+
+// // comparison operators
+// // let a = 5;
+// // let b = 10;
+// // console.log(a == b); // equal to
+
+// // console.log(a != b); // not equal to
+// // console.log(a === b); // strictly equal to
+// // console.log(a !== b); // strictly not equal to
+// // console.log(a > b); // greater than
+// // console.log(a < b); // less than
+// // console.log(a >= b); // greater than or equal to
+// // console.log(a <= b); // less than or equal to
+// // console.log(a == '5'); // equal to with type coercion
+// // console.log(a === '5'); // strictly equal to with type coercion
+// // console.log(a != '5'); // not equal to with type coercion
+// // console.log(a !== '5'); // strictly not equal to with type coercion
+// // console.log(a > '5'); // greater than with type coercion
+// // console.log(a < '5'); // less than with type coercion
+// // console.log(a >= '5'); // greater than or equal to with type coercion
+// // console.log(a <= '5'); // less than or equal to with type coercion
+// // console.log(a == 5); // equal to with type coercion
+
+
+
+// // logical operators
+// // let a = 5;
+// // let b = 10;
+// // console.log(a && b); // logical AND
+// // console.log(a || b); // logical OR
+// // console.log(!a); // logical NOT
+// // console.log(a && b > 5); // logical AND with comparison
+
+
+
+// //conditional statement
+// let age = 25;
+
+// if (age >= 18) {
+//     console.log("You are an adult.");
+// } else {
+//     console.log("You are a minor.");
+// }
+// // Ternary operator
+// age >= 18 ? console.log("You are an adult.") : console.log("You are a minor.");
+// // Switch statement
+// switch (age) {
+//     case 18:
+//         console.log("You are 18 years old.");
+//         break;
+//     case 25:
+//         console.log("You are 25 years old.");
+//         break;
+//     default:
+//         console.log("You are not 18 or 25 years old.");
+//         break;
+// };
+
+
+// let mode="white";
+// let color;
+// if (mode === "white") {
+//     color = "black";
+// }
+// else if (mode === "black") {
+//     color = "white";
+// } else {
+//     color = "gray";
+// }
+// console.log(color); // Output: black
+// // Ternary operator for the same logic
+// color = (mode === "white") ? "black" : (mode === "black") ? "white" : "gray";
+// console.log(color); // Output: black
+
+
+
+
+//alert("Hello, World!");
+
+
+// alert("hasnain wasli from kamalia");
+
+
+
+
+
+
+
+//prompt// let userInput = prompt("Please enter your name:");
+// console.log("User input:", userInput);
+  let age =prompt("Please enter your age:");
+  console.log("User age:", age);
