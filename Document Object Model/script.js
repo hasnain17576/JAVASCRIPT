567052785bff258e6ac3ea85c24e1d2f8a9999f7
hasnain17576 <hasnainwasli17@gmail.com>
2025-07-29 21:26:18 +0500

    submit

---
+++ Document Object Model/script.js
@@ -0,0 +1,10 @@
+// // console.dir(document.body);
+// // let heading = document.getElementById("title");
+
+// //   heading.innerText = "hasnain";
+
+
+// let messages = document.getElementsByClassName("msg");
+//   messages[0].style.color = "red"; // First msg red
+ let paras = document.getElementsByTagName("p");
+  paras[1].innerText = "Changed text";
